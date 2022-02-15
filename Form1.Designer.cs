9fb7931048d8849339c24542433a77b8f3c524db
Deatsilence <78795973+Sylar-Z31@users.noreply.github.com>
2022-02-15 22:19:47 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace CrongPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cboxLenght = new System.Windows.Forms.ComboBox();
+            this.lblPasswordLength = new System.Windows.Forms.Label();
+            this.rbtnEasy = new System.Windows.Forms.RadioButton();
+            this.rbtnMiddle = new System.Windows.Forms.RadioButton();
+            this.rbtnHard = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblPasswordSecurity = new System.Windows.Forms.Label();
+            this.btnMake = new System.Windows.Forms.Button();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.lblCreatedPassword = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboxLenght
+            // 
+            this.cboxLenght.BackColor = System.Drawing.Color.White;
+            this.cboxLenght.FormattingEnabled = true;
+            this.cboxLenght.Location = new System.Drawing.Point(160, 15);
+            this.cboxLenght.Name = "cboxLenght";
+            this.cboxLenght.Size = new System.Drawing.Size(50, 26);
+            this.cboxLenght.TabIndex = 0;
+            this.cboxLenght.SelectedIndexChanged += new System.EventHandler(this.cboxLenght_SelectedIndexChanged);
+            // 
+            // lblPasswordLength
+            // 
+            this.lblPasswordLength.AutoSize = true;
+            this.lblPasswordLength.BackColor = System.Drawing.Color.White;
+            this.lblPasswordLength.Location = new System.Drawing.Point(12, 15);
+            this.lblPasswordLength.Name = "lblPasswordLength";
+            this.lblPasswordLength.Size = new System.Drawing.Size(143, 18);
+            this.lblPasswordLength.TabIndex = 1;
+            this.lblPasswordLength.Text = "Password Length:";
+            // 
+            // rbtnEasy
+            // 
+            this.rbtnEasy.AutoSize = true;
+            this.rbtnEasy.BackColor = System.Drawing.Color.White;
+            this.rbtnEasy.Location = new System.Drawing.Point(6, 23);
+            this.rbtnEasy.Name = "rbtnEasy";
+            this.rbtnEasy.Size = new System.Drawing.Size(63, 22);
+            this.rbtnEasy.TabIndex = 2;
+            this.rbtnEasy.TabStop = true;
+            this.rbtnEasy.Text = "Easy";
+            this.rbtnEasy.UseVisualStyleBackColor = false;
+            this.rbtnEasy.Click += new System.EventHandler(this.CammonRadioButton_Click);
+            // 
+            // rbtnMiddle
+            // 
+            this.rbtnMiddle.AutoSize = true;
+            this.rbtnMiddle.BackColor = System.Drawing.Color.White;
+            this.rbtnMiddle.Location = new System.Drawing.Point(102, 23);
+            this.rbtnMiddle.Name = "rbtnMiddle";
+            this.rbtnMiddle.Size = new System.Drawing.Size(75, 22);
+            this.rbtnMiddle.TabIndex = 3;
+            this.rbtnMiddle.TabStop = true;
+            this.rbtnMiddle.Text = "Middle";
+            this.rbtnMiddle.UseVisualStyleBackColor = false;
+            this.rbtnMiddle.Click += new System.EventHandler(this.CammonRadioButton_Click);
+            // 
+            // rbtnHard
+            // 
+            this.rbtnHard.AutoSize = true;
+            this.rbtnHard.BackColor = System.Drawing.Color.White;
+            this.rbtnHard.Location = new System.Drawing.Point(189, 23);
+            this.rbtnHard.Name = "rbtnHard";
+            this.rbtnHard.Size = new System.Drawing.Size(62, 22);
+            this.rbtnHard.TabIndex = 4;
+            this.rbtnHard.TabStop = true;
+            this.rbtnHard.Text = "Hard";
+            this.rbtnHard.UseVisualStyleBackColor = false;
+            this.rbtnHard.Click += new System.EventHandler(this.CammonRadioButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.rbtnEasy);
+            this.groupBox1.Controls.Add(this.rbtnHard);
+            this.groupBox1.Controls.Add(this.rbtnMiddle);
+            this.groupBox1.Location = new System.Drawing.Point(216, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 60);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Password Difficulty";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(173, 91);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(339, 13);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // lblPasswordSecurity
+            // 
+            this.lblPasswordSecurity.AutoSize = true;
+            this.lblPasswordSecurity.BackColor = System.Drawing.Color.White;
+            this.lblPasswordSecurity.Location = new System.Drawing.Point(13, 86);
+            this.lblPasswordSecurity.Name = "lblPasswordSecurity";
+            this.lblPasswordSecurity.Size = new System.Drawing.Size(154, 18);
+            this.lblPasswordSecurity.TabIndex = 7;
+            this.lblPasswordSecurity.Text = "Password Security:";
+            // 
+            // btnMake
+            // 
+            this.btnMake.BackColor = System.Drawing.Color.White;
+            this.btnMake.ForeColor = System.Drawing.Color.Black;
+            this.btnMake.Location = new System.Drawing.Point(15, 147);
+            this.btnMake.Name = "btnMake";
+            this.btnMake.Size = new System.Drawing.Size(75, 27);
+            this.btnMake.TabIndex = 8;
+            this.btnMake.Text = "Create";
+            this.btnMake.UseVisualStyleBackColor = false;
+            this.btnMake.Click += new System.EventHandler(this.btnMake_Click);
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Location = new System.Drawing.Point(517, 91);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(0, 18);
+            this.lblPercent.TabIndex = 10;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // lblCreatedPassword
+            // 
+            this.lblCreatedPassword.AutoSize = true;
+            this.lblCreatedPassword.BackColor = System.Drawing.Color.White;
+            this.lblCreatedPassword.Location = new System.Drawing.Point(13, 118);
+            this.lblCreatedPassword.Name = "lblCreatedPassword";
+            this.lblCreatedPassword.Size = new System.Drawing.Size(171, 18);
+            this.lblCreatedPassword.TabIndex = 7;
+            this.lblCreatedPassword.Text = "Generated Password:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(192, 118);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(0, 18);
+            this.lblPassword.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(579, 186);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.btnMake);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblCreatedPassword);
+            this.Controls.Add(this.lblPasswordSecurity);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblPasswordLength);
+            this.Controls.Add(this.cboxLenght);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "CrongPassword";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblPasswordLength;
+        private System.Windows.Forms.RadioButton rbtnEasy;
+        private System.Windows.Forms.RadioButton rbtnMiddle;
+        private System.Windows.Forms.RadioButton rbtnHard;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblPasswordSecurity;
+        private System.Windows.Forms.Button btnMake;
+        private System.Windows.Forms.ComboBox cboxLenght;
+        private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lblCreatedPassword;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrongPassword
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //
+        // Assign password length to comboBox by difficulty
+        //
+        private static void AddComboBox(ref ComboBox cbox, int min, int max)
+        {
+            for (int i = min; i <= max; i++)
+                cbox.Items.Add(i);
+        }
+        //
+        // Nitially easy radioButton is selected and every radioButton makes a button
+        //
+        private static void GroupBoxControl(ref GroupBox gbox)
+        {
+            RadioButton rbtn = (RadioButton)gbox.Controls[0];
+            rbtn.Checked = true;
+
+            foreach (RadioButton item in gbox.Controls)
+                item.Appearance = Appearance.Button;
+        }
+        //
+        // Control radioButton
+        //
+        private static string RadioButtonControl(ref GroupBox gbox)
+        {
+            foreach (RadioButton item in gbox.Controls)
+            {
+                if (item.Checked)
+                    return item.Name;
+            }
+            return string.Empty;
+        }
+        //
+        // List of special characters, numbers, upper and lower case letters
+        //
+        private static List<char> Allascii = new List<char>(93);
+        //
+        // Numbers, upper and lower case letters list
+        //
+        private static List<char> notSpecialch = new List<char>(62);
+        //
+        // Importing characters to lists
+        //
+        private static void AddASCII()
+        {
+            for (int i = 33; i <= 125; i++)
+                Allascii.Add(Convert.ToChar(i));
+
+            for (int j = 48; j <= 122; j++)
+            {
+                if (char.IsLetterOrDigit(Convert.ToChar(j)))
+                    notSpecialch.Add(Convert.ToChar(j));
+            }
+        }
+        //
+        // Measures password security
+        //
+        private static int SecurityRating(ref char[] _password, ref ProgressBar progressBar)
+        {
+            int SumResult = 0;
+            for (int i = 0; i < _password.Length; i++)
+            {
+                if (char.IsUpper(_password[i]))
+                    SumResult += 2;
+
+                else if (char.IsLower(_password[i]))
+                    SumResult += 2;
+
+                else if (char.IsDigit(_password[i]))
+                    SumResult += 3;
+
+                else if (char.IsPunctuation(_password[i]))
+                    SumResult += 4;
+
+                else if (char.IsSymbol(_password[i]))
+                    SumResult += 6;
+
+            }
+            return SumResult;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.BackColor = Color.DarkGray;
+            this.lblPasswordLength.BackColor = Color.DarkGray;
+            this.lblPasswordSecurity.BackColor = Color.DarkGray;
+            this.lblCreatedPassword.BackColor = Color.DarkGray;
+            this.groupBox1.BackColor = Color.DarkGray;
+            this.rbtnEasy.BackColor = Color.DarkRed;
+            this.rbtnMiddle.BackColor = Color.YellowGreen;
+            this.rbtnHard.BackColor = Color.DarkGreen;
+            //
+            // AddASCII Method
+            //
+            AddASCII();
+            //
+            // btnMake
+            //
+            btnMake.Enabled = false;
+            btnMake.BackColor = Color.DarkOrange;
+            //
+            // groupBox1Controls
+            //
+            GroupBoxControl(ref groupBox1);
+            //
+            // CboxLenght
+            //
+            if (RadioButtonControl(ref groupBox1) == rbtnEasy.Name)
+                AddComboBox(ref cboxLenght, 4, 8);
+            else if (RadioButtonControl(ref groupBox1) == rbtnMiddle.Name)
+                AddComboBox(ref cboxLenght, 4, 16);
+            else if (RadioButtonControl(ref groupBox1) == rbtnHard.Name)
+                AddComboBox(ref cboxLenght, 8, 32);
+            cboxLenght.DropDownStyle = ComboBoxStyle.DropDownList;
+            //
+            // ProgessBar1
+            //
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            progressBar1.Enabled = false;
+            //
+            // toolTip
+            //
+            toolTip1.ShowAlways = true;
+            toolTip1.IsBalloon = true;
+            toolTip1.SetToolTip(this.lblPercent, "No password is 100% secure!");
+            toolTip1.SetToolTip(this.rbtnEasy, "4-8 character long password consisting of only uppercase and lowercase letters, and numbers repeated by characters.");
+            toolTip1.SetToolTip(this.rbtnMiddle, "4-16 character long password consisting of only upper and lower case letters, punctuation marks, symbols and numbers repeated by characters");
+            toolTip1.SetToolTip(this.rbtnMiddle, "12-32 character long password consisting of only uppercase and lowercase letters, punctuation marks, symbols and numbers without repeating characters.");
+        }
+
+        private void btnMake_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+
+            // password list of selected length
+            int capacity = Convert.ToInt32(cboxLenght.SelectedItem);
+            List<char> password = new List<char>(capacity);
+
+            // access to random index
+            Random rnd = new Random();
+            string result = string.Empty;
+
+            // if easy password is selected
+            if (RadioButtonControl(ref groupBox1) == rbtnEasy.Name)
+            {
+                for (int i = 0; i < capacity; i++)
+                {
+                    // random password generation
+                    int x = rnd.Next(0, notSpecialch.Count);
+                    password.Add(notSpecialch[x]);
+                }
+                result = new string(password.ToArray());
+                lblPassword.Text = result;
+
+                char[] temp = result.ToCharArray();
+
+                progressBar1.Value += (capacity + SecurityRating(ref temp, ref progressBar1));
+
+                lblPercent.Text = "% " + progressBar1.Value.ToString();
+            }
+            //
+            // if middle password is selected
+            //
+            else if (RadioButtonControl(ref groupBox1) == rbtnMiddle.Name)
+            {
+                for (int i = 0; i < capacity; i++)
+                {
+                    // random password generation
+                    int x = rnd.Next(0, Allascii.Count);
+                    password.Add(Allascii[x]);
+                }
+                result = new string(password.ToArray());
+                lblPassword.Text = result;
+
+                char[] temp = result.ToCharArray();
+                SecurityRating(ref temp, ref progressBar1);
+
+                int control = (capacity + SecurityRating(ref temp, ref progressBar1));
+
+                if (control >= 100)
+                    progressBar1.Value = 100;
+                else
+                    progressBar1.Value += control;
+
+                if (capacity > 12 && (progressBar1.Value + (capacity * 2)) < 100)
+                    progressBar1.Value += (capacity * 2);
+
+                lblPercent.Text = "% " + progressBar1.Value.ToString();
+            }
+            //
+            // if hard password is selected
+            //
+            else if (RadioButtonControl(ref groupBox1) == rbtnHard.Name)
+            {
+                for (int i = 0; i < capacity;)
+                {
+                    // random password generation
+                    int x = rnd.Next(0, Allascii.Count);
+                    // so that the character does not repeat
+                    if (!password.Contains(Allascii[x]))
+                    {
+                        password.Add(Allascii[x]);
+                        i++;
+                    }
+                }
+                result = new string(password.ToArray());
+                lblPassword.Text = result;
+
+                char[] temp = result.ToCharArray();
+                SecurityRating(ref temp, ref progressBar1);
+
+                int control = (capacity + SecurityRating(ref temp, ref progressBar1));
+
+                if (control >= 100)
+                    progressBar1.Value = 100;
+                else
+                    progressBar1.Value += control;
+
+                if (capacity > 16 && (progressBar1.Value + (capacity * 2)) < 100)
+                    progressBar1.Value += (capacity * 2);
+
+                lblPercent.Text = "% " + progressBar1.Value.ToString();
+            }
+            if (lblPassword.Text != string.Empty)
+                Clipboard.SetText(lblPassword.Text);
+        }
+
+        private void CammonRadioButton_Click(object sender, EventArgs e)
+        {
+            if (RadioButtonControl(ref groupBox1) == rbtnEasy.Name)
+            {
+                cboxLenght.Items.Clear();
+                AddComboBox(ref cboxLenght, 4, 8);
+            }
+            else if (RadioButtonControl(ref groupBox1) == rbtnMiddle.Name)
+            {
+                cboxLenght.Items.Clear();
+                AddComboBox(ref cboxLenght, 4, 16);
+            }
+
+            else if (RadioButtonControl(ref groupBox1) == rbtnHard.Name)
+            {
+                cboxLenght.Items.Clear();
+                AddComboBox(ref cboxLenght, 12, 32);
+            }
+            if (cboxLenght.SelectedIndex > -1)
+                btnMake.Enabled = true;
+            else
+                btnMake.Enabled = false;
+        }
+
+        private void cboxLenght_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboxLenght.SelectedIndex > -1)
+                btnMake.Enabled = true;
+            else
+                btnMake.Enabled = false;
+        }
+    }
+}
